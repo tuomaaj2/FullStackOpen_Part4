7507cf771c6ad94298ac6c36f70fbaf69880dde6
Juho-Topias Tuomaala <juho-topias@hotmail.com>
2021-11-17 14:11:47 +0200

    4.14 completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,4 +18,20 @@ blogsRouter.post('/', async (request, response) => {
 
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogs = await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    likes: body.likes,
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
