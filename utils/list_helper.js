41cf16734b24ae4cfbebfb1bc768f6925b353522
Juho-Topias Tuomaala <juho-topias@hotmail.com>
2021-11-30 16:56:20 +0200

    4.15 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -62,10 +64,16 @@ const mostLikes = (blogs) => {
   return ret_obj
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
     mostBlogs,
-    mostLikes
+    mostLikes,
+    usersInDb,
 }
